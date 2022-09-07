8222298c046ff9fb7f5c0fe5c6e6c7b0078b64d8
Ingo Ziegler <ziegler.ingo@gmail.com>
2022-09-08 00:17:56 +0200

    add divide_mix

---
+++ denoise/divide_mix/data_handling.py
@@ -0,0 +1,274 @@
+"""
+Customised implementation of the original paper:
+    DivideMix: Learning With Noisy Labels as Semi-Supervised Learning
+    https://openreview.net/pdf?id=HJgExaVtwr
+    https://github.com/LiJunnan1992/DivideMix
+"""
+
+
+import csv
+import numpy as np
+
+import torch
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+
+
+# load vocab and encoder as globals
+vocab = torch.load('../data/vocab.pt')
+encode_text = lambda x: vocab(list(x))
+
+
+def read_data(path):
+    with open(path, 'r') as csvfile:
+        train_data = list(csv.reader(csvfile))[1:] # skip col name
+        sents, lbls = [], []
+        for s, l in train_data:
+            sents.append(s)
+            lbls.append(l)
+    return sents, lbls
+
+def apply_random_masking(seq, num_tokens):
+    """
+    Mask `num_tokens` as 0 at random positions per sequence. 
+    """
+    dist = torch.rand(seq.shape)
+    m, _ = torch.topk(dist, num_tokens)
+    return seq * (dist < m)
+
+
+class CleavageDataset(Dataset):
+    def __init__(
+        self,
+        terminus,
+        mode,
+        pred=None,
+        probability=None,
+    ):
+
+        self.mode = mode
+
+        if self.mode == "test":
+            self.seq, self.lbl = read_data(f"../data/{terminus}_test.csv")
+        elif self.mode == 'evaluate':
+            self.seq, self.lbl = read_data(f"../data/{terminus}_val.csv")
+        else:
+            sequences, labels = read_data(f"../data/{terminus}_train.csv")
+
+            if self.mode == "train_before_GMM":
+                self.seq, self.lbl = sequences, labels
+            elif self.mode == "labeled" and pred is not None and probability is not None:
+                # get indices of labels considered 'clean' by GMM
+                pred_idx = np.nonzero(pred)[0]
+                self.prob = probability[pred_idx]
+                self.seq, self.lbl = np.array(sequences)[pred_idx], np.array(labels)[pred_idx]
+ 
+            elif self.mode == "unlabeled" and pred is not None:
+                # get indices of labels considered 'noisy' by GMM
+                pred_idx = np.nonzero(~pred)[0]
+                self.seq = np.array(sequences)[pred_idx] 
+            else:
+                raise Exception(
+                    "Either args `pred` and/or `probability` need to be given when loading `mode`='train'."
+                )
+
+    def __getitem__(self, idx):
+        if self.mode == "labeled":
+            return self.seq[idx], self.lbl[idx], self.prob[idx]
+
+        elif self.mode == "unlabeled":
+            return self.seq[idx]
+
+        else: 
+            return self.seq[idx], self.lbl[idx] 
+
+    def __len__(self):
+        return len(self.seq)
+
+
+class MaskedNoGMMBatch:
+    def __init__(self, batch):
+        ordered_batch = list(zip(*batch))
+        raw_seq = torch.tensor(
+            [encode_text(seq) for seq in ordered_batch[0]], dtype=torch.int64
+        )
+        self.seq = apply_random_masking(raw_seq, 1)
+        lbl = torch.tensor([int(l) for l in ordered_batch[1]], dtype=torch.long)
+        self.lbl = F.one_hot(lbl, num_classes=2).to(torch.float)
+
+    def pin_memory(self):
+        self.seq = self.seq.pin_memory()
+        self.lbl = self.lbl.pin_memory()
+        return self
+
+
+def masked_no_gmm_wrapper(batch):
+    return MaskedNoGMMBatch(batch)
+
+
+class LabeledMaskedBatch:
+    def __init__(self, batch):
+        ordered_batch = list(zip(*batch))
+        raw_seq = torch.tensor(
+            [encode_text(seq) for seq in ordered_batch[0]], dtype=torch.int64
+        )
+        self.seq1 = apply_random_masking(raw_seq, 1)
+        self.seq2 = apply_random_masking(raw_seq, 1)
+        lbl = torch.tensor([int(l) for l in ordered_batch[1]], dtype=torch.long)
+        self.lbl = F.one_hot(lbl, num_classes=2).to(torch.float)
+        self.prob = torch.tensor(ordered_batch[2], dtype=torch.float)
+
+    def pin_memory(self):
+        self.seq1 = self.seq1.pin_memory()
+        self.seq2 = self.seq2.pin_memory()
+        self.lbl = self.lbl.pin_memory()
+        self.prob = self.prob.pin_memory()
+        return self
+
+
+def labeled_masked_wrapper(batch):
+    return LabeledMaskedBatch(batch)
+
+
+class UnlabeledMaskedBatch:
+    def __init__(self, batch):
+        # ordered_batch = list(zip(*batch))
+        raw_seq = torch.tensor(
+            [encode_text(seq) for seq in batch], dtype=torch.int64
+        )
+        self.seq1 = apply_random_masking(raw_seq, 1)
+        self.seq2 = apply_random_masking(raw_seq, 1)
+
+    def pin_memory(self):
+        self.seq1 = self.seq1.pin_memory()
+        self.seq2 = self.seq2.pin_memory()
+        return self
+
+
+def unlabeled_masked_wrapper(batch):
+    return UnlabeledMaskedBatch(batch)
+
+
+class EvalBatch:
+    def __init__(self, batch):
+        ordered_batch = list(zip(*batch))
+        self.seq = torch.tensor(
+            [encode_text(seq) for seq in ordered_batch[0]], dtype=torch.int64
+        )
+        lbl = torch.tensor([int(l) for l in ordered_batch[1]], dtype=torch.long)
+        self.lbl = F.one_hot(lbl, num_classes=2).to(torch.float)
+
+    def pin_memory(self):
+        self.seq = self.seq.pin_memory()
+        self.lbl = self.lbl.pin_memory()
+        return self
+
+
+def eval_wrapper(batch):
+    return EvalBatch(batch)
+
+
+class CleavageLoader:
+    def __init__(
+        self,
+        batch_size,
+        num_workers,
+    ):
+
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+    def load(self, terminus, mode, pred=None, prob=None):
+        if mode == "warmup":
+            train_no_gmm = CleavageDataset(
+                terminus=terminus,
+                mode="train_before_GMM",
+            )
+
+            warmup_loader = DataLoader(
+                dataset=train_no_gmm,
+                batch_size=self.batch_size * 8,
+                shuffle=True,
+                collate_fn=masked_no_gmm_wrapper,
+                pin_memory=True,
+                num_workers=self.num_workers,
+            )
+            return warmup_loader
+
+        elif mode == "train":
+            labeled_dataset = CleavageDataset(
+                terminus=terminus, mode="labeled", pred=pred, probability=prob
+            )
+
+            labeled_trainloader = DataLoader(
+                dataset=labeled_dataset,
+                batch_size=self.batch_size,
+                shuffle=True,
+                collate_fn=labeled_masked_wrapper,
+                pin_memory=True,
+                num_workers=self.num_workers,
+            )
+
+            unlabeled_dataset = CleavageDataset(
+                terminus=terminus,
+                mode="unlabeled",
+                pred=pred,
+            )
+
+            unlabeled_trainloader = DataLoader(
+                dataset=unlabeled_dataset,
+                batch_size=self.batch_size,
+                shuffle=True,
+                collate_fn=unlabeled_masked_wrapper,
+                pin_memory=True,
+                num_workers=self.num_workers,
+            )
+            return labeled_trainloader, unlabeled_trainloader
+
+        elif mode == "divide_by_GMM":
+            into_GMM_dataset = CleavageDataset(
+                terminus=terminus,
+                mode="train_before_GMM",
+            )
+
+            into_GMM_loader = DataLoader(
+                dataset=into_GMM_dataset,
+                batch_size=self.batch_size * 16,
+                collate_fn=eval_wrapper,
+                pin_memory=True,
+                shuffle=False,
+                num_workers=self.num_workers,
+            )
+            return into_GMM_loader
+
+        elif mode == "evaluate":
+            eval_dataset = CleavageDataset(
+                terminus=terminus,
+                mode="evaluate",
+            )
+
+            eval_loader = DataLoader(
+                dataset=eval_dataset,
+                batch_size=self.batch_size * 16,
+                collate_fn=eval_wrapper,
+                pin_memory=True,
+                shuffle=False,
+                num_workers=self.num_workers,
+            )
+            return eval_loader
+
+        elif mode == "test":
+            test_dataset = CleavageDataset(
+                terminus=terminus,
+                mode="test",
+            )
+
+            test_loader = DataLoader(
+                dataset=test_dataset,
+                batch_size=self.batch_size * 16,
+                collate_fn=eval_wrapper,
+                pin_memory=True,
+                shuffle=False,
+                num_workers=self.num_workers,
+            )
+            return test_loader
---
+++ denoise/divide_mix/train_utils.py
@@ -0,0 +1,246 @@
+import sys
+import numpy as np
+from tqdm import tqdm
+
+import torch
+import torch.nn.functional as F
+
+from sklearn.metrics import roc_auc_score
+from sklearn.mixture import GaussianMixture
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+
+class SemiLoss:
+    def __call__(self, out_x, lbl_x, out_u, lbl_u, lambda_u, epoch, warm_up):
+        probs_u = torch.softmax(out_u, dim=1)
+
+        Lx = -torch.mean(torch.sum(F.log_softmax(out_x, dim=1) * lbl_x, dim=1))
+        Lu = torch.mean((probs_u - lbl_u) ** 2)
+        return Lx, Lu, linear_rampup(lambda_u, epoch, warm_up)
+
+
+class NegEntropy:
+    def __call__(self, out):
+        probs = torch.softmax(out, dim=1)
+        return torch.mean(torch.sum(probs.log() * probs, dim=1))
+
+
+def warmup(model, optimizer, loss_func, conf_penalty, dataloader):
+    epoch_loss, num_correct, total = 0, 0, 0
+    preds, lbls = [], []
+    model.train()
+
+    for batch in tqdm(dataloader, desc="warmup: ", file=sys.stdout, unit="batches"):
+        in_x, lbl_x = batch.seq, batch.lbl
+        in_x, lbl_x = in_x.to(device), lbl_x.to(device)
+
+        out = model(in_x)
+        # CrossEntropyLoss
+        loss = loss_func(out, lbl_x)
+
+        # penalty for confident predictions for asymmetric noise
+        loss += conf_penalty(out)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        epoch_loss += loss.item()
+        non_ohe_labels = lbl_x.argmax(dim=1)
+        num_correct += (out.argmax(dim=1) == non_ohe_labels).sum().item()
+        total += in_x.shape[0]
+        preds.extend(out[:, 1].detach().tolist())
+        lbls.extend(non_ohe_labels.detach().tolist())
+    return epoch_loss / total, num_correct / total, roc_auc_score(lbls, preds)
+
+
+def train(
+    epoch,
+    model1,
+    model2,
+    optimizer,
+    loss_func,
+    num_warm_up_epochs,
+    num_classes,
+    temp,
+    beta_dist,
+    batch_size,
+    labeled_loader,
+    unlabeled_loader,
+    named_model,
+):
+    epoch_loss, total = 0, 0
+
+    # fix one model while the other one trains
+    model1.train()
+    model2.eval()
+
+    unlabeled_train_iter = iter(unlabeled_loader)
+    num_iter = (len(labeled_loader.dataset) // batch_size) + 1
+
+    for batch_idx, batch in enumerate(
+        tqdm(
+            labeled_loader,
+            desc=f"train {named_model}: ",
+            file=sys.stdout,
+            unit="batches",
+        )
+    ):
+        in_x, in_x2, labels_x, w_x = batch.seq1, batch.seq2, batch.lbl, batch.prob
+
+        # re-use unlabeled_loader as long as there are batches in labeled_loader
+        try:
+            unlabeled_batch = unlabeled_train_iter.next()
+            in_u, in_u2 = unlabeled_batch.seq1, unlabeled_batch.seq2
+        except:
+            unlabeled_train_iter = iter(unlabeled_loader)
+            unlabeled_batch = unlabeled_train_iter.next()
+            in_u, in_u2 = unlabeled_batch.seq1, unlabeled_batch.seq2
+
+        w_x = w_x.view(-1, 1).to(torch.float)
+        in_x, in_x2, labels_x, w_x = (
+            in_x.to(device),
+            in_x2.to(device),
+            labels_x.to(device),
+            w_x.to(device),
+        )
+
+        in_u, in_u2 = in_u.to(device), in_u2.to(device)
+
+        with torch.no_grad():
+            # for labeled samples: co-refinement + temperature sharpening
+            out_x = model1(in_x)
+            out_x2 = model1(in_x2)
+
+            px = (torch.softmax(out_x, dim=1) + torch.softmax(out_x2, dim=1)) / 2
+            px = w_x * labels_x + (1 - w_x) * px
+
+            ptx = px ** (1 / temp)
+            targets_x = (ptx / ptx.sum(dim=1, keepdim=True)).detach()
+
+            # for unlabeled samples: co-guessing + temperature sharpening
+            out_u11 = model1(in_u)
+            out_u12 = model1(in_u2)
+            out_u21 = model2(in_u)
+            out_u22 = model2(in_u2)
+
+            pu = (
+                torch.softmax(out_u11, dim=1)
+                + torch.softmax(out_u12, dim=1)
+                + torch.softmax(out_u21, dim=1)
+                + torch.softmax(out_u22, dim=1)
+            ) / 4
+
+            ptu = pu ** (1 / temp)
+            targets_u = (ptu / ptu.sum(dim=1, keepdim=True)).detach()
+
+        ### MixMatch
+        # lambda interpolation factor for mixed input and targets
+        lam = beta_dist.sample()
+        lam = max(lam, 1 - lam)
+
+        all_ins = torch.cat([in_x, in_x2, in_u, in_u2], dim=0)
+        all_targets = torch.cat([targets_x, targets_x, targets_u, targets_u], dim=0)
+
+        # shuffle all inputs to generate new pairs
+        idx = torch.randperm(all_ins.shape[0])
+
+        in_a, in_b = all_ins, all_ins[idx]
+        target_a, target_b = all_targets, all_targets[idx]
+
+        mixed_target = lam * target_a + (1 - lam) * target_b
+        # inputs are mixed up in forward pass
+        logits = model1(in_a, in_b, lam, interpolate=True)
+        logits_x, logits_u = logits[: batch_size * 2], logits[batch_size * 2 :]
+
+        # custom SemiLoss
+        Lx, Lu, lambda_u = loss_func(
+            logits_x,
+            mixed_target[: batch_size * 2],
+            logits_u,
+            mixed_target[batch_size * 2 :],
+            epoch + batch_idx / num_iter,
+            num_warm_up_epochs,
+        )
+
+        # regularization
+        prior = (torch.ones(num_classes) / num_classes).to(device)
+        pred_mean = torch.softmax(logits, dim=1).mean(0)
+        penalty = torch.sum(prior * torch.log(prior / pred_mean))
+
+        loss = Lx + lambda_u * Lu + penalty
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        epoch_loss += loss.item()
+        total += idx.shape[0]
+    return epoch_loss / total
+
+
+def process_gmm(model, dataloader, loss_func):
+    losses = []
+    model.eval()
+
+    with torch.no_grad():
+        for batch in tqdm(
+            dataloader, desc="GMM processing: ", file=sys.stdout, unit="batches"
+        ):
+            in_x, lbl_x = batch.seq, batch.lbl
+            in_x, lbl_x = in_x.to(device), lbl_x.to(device)
+
+            out = model(in_x)
+            # nn.CrossEntropyLoss with reduction=None, returns per sample loss
+            loss = loss_func(out, lbl_x)
+            losses.extend(loss.detach().tolist())
+
+    losses = np.array(losses)
+    # normalize losses between 0 and 1
+    norm_losses = ((losses - losses.min()) / losses.ptp())[:, np.newaxis]
+
+    # fit two component GMM to loss
+    gmm = GaussianMixture(n_components=2, max_iter=10, tol=1e-2, reg_covar=5e-4)
+    gmm.fit(norm_losses)
+    prob = gmm.predict_proba(norm_losses)
+    # get value of smaller mean dist
+    prob = prob[:, gmm.means_.argmin()]
+    # out shape: (batch_size)
+    return prob, losses.mean()
+
+
+def evaluate(model1, model2, loss_func, dataloader):
+    num_correct, total = 0, 0
+    preds, lbls, losses = [], [], []
+    model1.eval()
+    model2.eval()
+
+    with torch.no_grad():
+        for batch in tqdm(
+            dataloader, desc="evaluate: ", file=sys.stdout, unit="batches"
+        ):
+            in_x, lbl_x = batch.seq, batch.lbl
+            in_x, lbl_x = in_x.to(device), lbl_x.to(device)
+            out1 = model1(in_x)
+            out2 = model2(in_x)
+            out = out1 + out2
+
+            # nn.CrossEntropyLoss with reduction=None, returns per sample loss
+            loss = loss_func(out, lbl_x)
+            losses.extend(loss.detach().tolist())
+
+            non_ohe_labels = lbl_x.argmax(dim=1)
+            num_correct += (out.argmax(dim=1) == non_ohe_labels).sum().item()
+            total += in_x.shape[0]
+            preds.extend(out[:, 1].detach().tolist())
+            lbls.extend(non_ohe_labels.detach().tolist())
+
+    losses = np.array(losses)
+    return num_correct / total, roc_auc_score(lbls, preds), losses.mean()
+
+
+def linear_rampup(lambda_u, current_epoch, warm_up, rampup_len=15):
+    current = np.clip((current_epoch - warm_up) / rampup_len, 0.0, 1.0)
+    return lambda_u * current
+
+
